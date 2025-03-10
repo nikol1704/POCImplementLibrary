f77a9a30b479a66215b2901d31e755f3de844777
nikol.1704 <nikol.lazarte@540deg.com>
2025-03-10 11:44:32 +0100

    create project

---
+++ POCImplementlibrary/POCImplementlibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCImplementlibraryApp.swift
+//  POCImplementlibrary
+//
+//  Created by Nikol on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct POCImplementlibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
